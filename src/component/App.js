74848783c46792a2073597887de9aa3de86b5549
Ubuntu <ubuntu@ip-172-31-92-72.ec2.internal>
2021-01-27 17:26:52 +0000

    Added code icon

--- src/component/App.js
+++ src/component/App.js
@@ -4,7 +4,8 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
-import {MenuIcon, CodeIcon} from '@material-ui/icons';
+import CodeIcon from '@material-ui/icons/Code';
+import MenuIcon from '@material-ui/icons/Menu';
 
 const styles = {
   root: {
