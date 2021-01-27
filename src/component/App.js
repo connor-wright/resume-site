8021730ef21b767a164515c846622889bf10f1e4
Connor Wright <connor.d.wright1996@gmail.com>
2021-01-27 17:02:38 +0000

    attempting to add material ui

--- src/component/App.js
+++ src/component/App.js
@@ -1,10 +1,41 @@
 import React from "react";
+import { withStyles} from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import {MenuIcon, CodeIcon} from '@material-ui/icons';
 
-export default class App extends React.Component {
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: '10px',
+  },
+};
+
+class App extends React.Component {
   render() {
+    const {classes} = this.props;
+    
     return (
-      <div>Hello World</div>
+      <AppBar position="static">
+        <Toolbar variant="dense">
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" color="inherit">
+            Photos
+          </Typography>
+          <IconButton edge="end" color="inherit" aria-label="source code">
+            <CodeIcon/>
+          </IconButton>
+        </Toolbar>
+      </AppBar>
     );
   }
 }
+
+export default withStyles(styles)(App);
 
