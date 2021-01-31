8acb439ab4fdf176bb8cce2d9fcbd15e454f4b3a
Connor Wright <connor.d.wright1996@gmail.com>
2021-01-31 05:11:43 +0000

    Added header

--- src/component/App.js
+++ src/component/App.js
@@ -5,7 +5,7 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import CodeIcon from '@material-ui/icons/Code';
-import MenuIcon from '@material-ui/icons/Menu';
+import GitHubIcon from '@material-ui/icons/GitHub';
 
 const styles = {
   root: {
@@ -14,6 +14,9 @@ const styles = {
   menuButton: {
     marginRight: '10px',
   },
+  spacer: {
+    flexGrow: 1,
+  }
 };
 
 class App extends React.Component {
@@ -21,19 +24,48 @@ class App extends React.Component {
     const {classes} = this.props;
     
     return (
-      <AppBar position="static">
-        <Toolbar variant="dense">
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" color="inherit">
-            Photos
-          </Typography>
-          <IconButton edge="end" color="inherit" aria-label="source code">
-            <CodeIcon/>
-          </IconButton>
-        </Toolbar>
-      </AppBar>
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar variant="dense">
+            <IconButton className={classes.menuButton} edge="start" color="inherit" aria-label="about-me">
+              <Typography variant="h6" color="inherit">
+                About Me
+              </Typography>
+            </IconButton>
+            <IconButton className={classes.menuButton} edge="start" color="inherit" aria-label="projects">
+              <Typography variant="h6" color="inherit">
+                Projects
+              </Typography>
+            </IconButton>
+            <IconButton className={classes.menuButton} edge="start" color="inherit" aria-label="more">
+              <Typography variant="h6" color="inherit">
+                More
+              </Typography>
+            </IconButton>
+            <div className={classes.spacer}/>
+            <IconButton 
+              className={classes.menuButton} 
+              href="https://github.com/connor-wright"
+              target="_blank"
+              edge="start" 
+              color="inherit" 
+              aria-label="github"
+            >
+              <GitHubIcon/>
+            </IconButton>
+            <IconButton 
+              className={classes.menuButton} 
+              href="https://github.com/connor-wright/resume-site"
+              target="_blank"
+              edge="start" 
+              color="inherit" 
+              aria-label="source-code"
+            >
+              <CodeIcon/>
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+      </div>
     );
   }
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,10 @@ import * as React from "react";
 import App from "../component/App";
 import "fontsource-roboto";
 
+// set body style
+document.body.style.margin = "0px";
 // markup
 const IndexPage = () => {
-  
   return (
     <App />
   );
