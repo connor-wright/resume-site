0cdf8c20ab90fbf926b8a5e53c93c36458c383e8
Connor Wright <connor.d.wright1996@gmail.com>
2021-01-31 06:31:43 +0000

    Fixed root refrence issues with react dom/gatsby

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,10 @@
 import * as React from "react";
 import App from "../component/App";
 import "fontsource-roboto";
-import ReactDom from "react-dom";
 
 const IndexPage = () => {
-  return ReactDom.render(
-    <App />,
-    document.getElementById("___gatsby")
+  return (
+    <App />
   );
 };
 
